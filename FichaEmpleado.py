e89356f0050559f0b88974d5c028a0a2c3a6dcf8
Hector Patiño <hectorpatino24@gmail.com>
2020-04-25 17:54:39 -0500

    Se modifica la clase Cliente, se cambia el nombre por FichaEmpleado y se añaden los atributos antiguedad y cualificacion

---
+++ FichaEmpleado.py
@@ -0,0 +1,6 @@
+class FichaEmpleado():
+    def __init__(self, nombre, edad, antiguedad, cualificacion):
+        self.nombre = nombre
+        self.edad = edad
+        self.antiguedad = antiguedad
+        self.cualificacion = cualificacion
---
+++ FichaFabricacion.py
@@ -0,0 +1,7 @@
+class FichaFabricacion():
+    def __init__(self, articulos_mes):
+        self.articulos_mes = articulos_mes
+
+    def ficha_fabricacion(self):
+        pass
+        
