203af5bd4c82f861f33cbe06e67df7223dedd7d1
Hector Patiño <hectorpatino24@gmail.com>
2020-04-26 12:22:24 -0500

    Se crea el método sueldo de la clase FichaEmpleado

--- FichaEmpleado.py
+++ FichaEmpleado.py
@@ -4,3 +4,6 @@ class FichaEmpleado():
         self.edad = edad
         self.antiguedad = antiguedad
         self.cualificacion = cualificacion
+
+    def __sueldo(self):
+        return (1000 + self.antiguedad * 25 + self.cualificacion * 100)
