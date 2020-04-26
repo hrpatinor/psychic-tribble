6989fed1b9ecbc13438d8e5e872b797c8e5469cf
Hector Patiño <hectorpatino24@gmail.com>
2020-04-26 13:53:46 -0500

    Se añade FichaFabricación como herencia de Ficha Empleado y la herencia en la clase

--- FichaFabricacion.py
+++ FichaFabricacion.py
@@ -1,8 +1,10 @@
-class FichaFabricacion():
+from FichaEmpleado import FichaEmpleado
+class FichaFabricacion(FichaEmpleado):
     def __init__(self, articulos_mes):
+        super().__init__()
         self.articulos_mes = articulos_mes
 
     def inc_articulos(self, suma):
         self.articulos_mes += suma
-        
+
         
