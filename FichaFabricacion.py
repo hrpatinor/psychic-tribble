f6da3e9393793e37df0b99290d2188c9e229afb9
Hector Patiño <hectorpatino24@gmail.com>
2020-04-26 12:45:01 -0500

    Se añade el método de inc_articulos a Ficha Fabricacion

--- FichaFabricacion.py
+++ FichaFabricacion.py
@@ -2,6 +2,7 @@ class FichaFabricacion():
     def __init__(self, articulos_mes):
         self.articulos_mes = articulos_mes
 
-    def ficha_fabricacion(self):
-        pass
+    def inc_articulos(self, suma):
+        self.articulos_mes += suma
+        
         
